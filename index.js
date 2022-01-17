8e5bf6588679e101f45a3c37d0931fe830fb3c90
mckaymatt3 <mckaymatt@outlook.com>
2022-01-16 22:17:08 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (a,b) {
+    let result = a + b
+        return result
+}
+
+function subtract (a,b) {
+    let result = a - b
+        return result
+}
+
+function multiply (a,b) {
+    let result = a * b
+        return result
+}
+
+function divide (a,b) {
+    let result = a / b
+        return result
+}
+
+function increment (n) {
+    let result = n + 1
+        return result
+}
+
+function decrement (n) {
+    let result = n - 1
+        return result
+}
+
+function makeInt (n) {
+    let result = parseInt(n,10)
+        return result
+}
+
+function preserveDecimal (n) {
+    let result = parseFloat(n)
+        return result
+}
